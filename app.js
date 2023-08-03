db291fa7a622a44835a35c8541865b5215c5439e
rickyhuangjh <103763393+rickyhuangjh@users.noreply.github.com>
2023-08-03 05:04:23 -0400

    s

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 document.getElementById('button-lightDark').addEventListener('click', function() {
     const body = document.body;
+    const menuBar = document.querySelector('.menu-bar');
     const menuList = document.querySelectorAll('.menu');
     const lightDarkIcon = document.getElementById('icon-lightDark');
+    const sectionList = document.querySelectorAll('section');
+
 
 
     body.classList.toggle('light-mode');
+    menuBar.classList.toggle('light-mode');
     menuList.forEach((x) => x.classList.toggle('light-mode'));
+    sectionList.forEach((x) => x.classList.toggle('light-mode'));
 
     if (body.classList.contains('light-mode')) {
         lightDarkIcon.innerHTML = "light_mode";
