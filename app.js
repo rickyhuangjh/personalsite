7e213f2eef0a076394b3e9c8d81a5ba06395c3b2
rickyhuangjh <103763393+rickyhuangjh@users.noreply.github.com>
2023-08-04 01:42:46 -0400

    s

--- app.js
+++ app.js
@@ -22,26 +22,4 @@ document.getElementById('button-lightDark').addEventListener('click', function()
     return;
 
 
-    if (!body.classList.contains('background-dark-mode')) {
-        lightDarkButton.innerHTML = "Dark Mode";
-
-        for (let i=0; i<menuList.length; i++) {
-            menuList[i].classList.remove('menu-light-mode');
-            menuList[i].classList.add('menu-dark-mode');
-        }
-
-        body.classList.remove('background-light-mode');
-        body.classList.add('background-dark-mode');
-    } else {
-        lightDarkButton.innerHTML = "Light Mode";
-
-        for (let i=0; i<menuList.length; i++) {
-            menuList[i].classList.remove('menu-dark-mode');
-            menuList[i].classList.add('menu-light-mode');
-        }
-
-
-        body.classList.remove('background-dark-mode');
-        body.classList.add('background-light-mode');
-    }
   });
